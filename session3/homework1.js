55ced793272ad9909191ce2a1a60aeb42328ceb8
maixuansang <xuansang163405@gmail.com>
2019-07-16 16:24:28 +0700

    initial commit

---
+++ session3/homework1.js
@@ -0,0 +1,26 @@
+let menu = ["T-Shirt", "Sweater"];
+
+menu.forEach((clothes, index) => {
+    console.log(`${index+1}. ${clothes}`);
+})
+
+let answer = promt("Welcome to our shop, what do you want (C, R, U, D)?")
+
+if (answer === `R`){
+for(let i = 0; i < menu.length; i++){
+    console.log(menu[i])
+}
+}else if (answer === `C`){
+    let newclothes = prompt("Clothes name?");
+    menu.push(newclothes);
+}
+else if(answer === `U`){
+    menu[1] = "Skirt";
+    console.log(menu)
+}else if(answer === `D`){
+    menu.splice(2, 1);
+console.log(menu);
+    }
+    
+
+   
